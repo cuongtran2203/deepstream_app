c5066d65cfe65dd57d8240b18bfbd8c6b195f1be
Trần Đình Mạnh Cường <81034438+cuongtran2203@users.noreply.github.com>
2022-08-01 16:54:00 +0700

    Add files via upload

---
+++ buffer2numpy.py
@@ -0,0 +1,477 @@
+from copy import copy
+import sys
+sys.path.append('../')
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import GLib, Gst
+from common.is_aarch_64 import is_aarch64
+from common.bus_call import bus_call
+from ctypes import *
+import time
+import sys
+import math
+import configparser
+import platform
+from common.is_aarch_64 import is_aarch64
+from common.bus_call import bus_call
+from common.FPS import PERF_DATA
+import pyds
+import cv2
+import numpy as np
+import os
+from os import path
+perf_data=None
+perf_data = None
+frame_count = {}
+saved_count = {}
+MIN_CONFIDENCE = 0.2
+global PGIE_CLASS_ID_VEHICLE
+PGIE_CLASS_ID_VEHICLE = 0
+global PGIE_CLASS_ID_PERSON
+PGIE_CLASS_ID_PERSON = 2
+past_tracking_meta=[0]
+
+MAX_DISPLAY_LEN = 64
+PGIE_CLASS_ID_VEHICLE = 0
+PGIE_CLASS_ID_BICYCLE = 1
+PGIE_CLASS_ID_PERSON = 2
+PGIE_CLASS_ID_ROADSIGN = 3
+MUXER_OUTPUT_WIDTH = 1280
+MUXER_OUTPUT_HEIGHT = 720
+MUXER_BATCH_TIMEOUT_USEC = 4000000
+TILED_OUTPUT_WIDTH = 1280
+TILED_OUTPUT_HEIGHT = 720
+GST_CAPS_FEATURES_NVMM = "memory:NVMM"
+pgie_classes_str = ["Vehicle", "TwoWheeler", "Person", "RoadSign"]
+MAX_CONFIDENCE = 0.4
+def buffer2numpy_image_probe(pad,info,u_data):
+    frame_number=0
+   
+    obj_counter={
+        PGIE_CLASS_ID_VEHICLE:0,
+        PGIE_CLASS_ID_PERSON:0,
+        PGIE_CLASS_ID_BICYCLE:0,
+        PGIE_CLASS_ID_ROADSIGN:0
+    }
+    num_rects=0
+    gst_buffer=info.get_buffer()
+    if not gst_buffer:
+        print("Unable to get GstBuffer")
+        return
+    batch_meta=pyds.gst_buffer_get_nvds_batch_meta(hash(gst_buffer))
+    l_frame=batch_meta.frame_meta_list
+    while l_frame is not None:
+        try:
+            frame_meta=pyds.NvDsFrameMeta.cast(l_frame.data)
+        except StopIteration:
+            break
+        is_first_obj=True
+        save_image=False
+        frame_number=frame_meta.frame_num
+        num_rects=frame_meta.num_obj_meta
+        l_obj=frame_meta.obj_meta_list
+        while l_obj is not None:
+            try:
+                obj_meta=pyds.NvDsObjectMeta.cast(l_obj.data)
+            except StopIteration:
+                break
+            obj_counter[obj_meta.class_id]+=1
+            ### convert buffer to array
+            if saved_count["stream_{}".format(frame_meta.pad_index)] %30==0 and(
+                MIN_CONFIDENCE<obj_meta.confidence <MAX_CONFIDENCE):
+                if is_first_obj:
+                    is_first_obj=False
+                    #getting Image data using nvbufsurface
+                    # the input should be address of buffer and batch_id
+                    n_frame=pyds.get_nvds_buf_surface(hash(gst_buffer),frame_meta.batch_id)
+                    frame_copy=np.array(n_frame,copy=True,order='C')
+                    #print(frame_copy)
+                    #convert the array into cv2 default color format
+                    frame_copy = cv2.cvtColor(frame_copy,cv2.COLOR_RGBA2BGRA)
+                save_image=True
+                    #########################################################
+            
+            
+            try:
+                l_obj=l_obj.next
+            except StopIteration:
+                break
+            #### visual text on screen
+      
+        display_meta=pyds.nvds_acquire_display_meta_from_pool(batch_meta)
+        display_meta.num_labels=1
+        py_nvosd_text_param=display_meta.text_params[0]
+        ##Setting display text to be shown on screen 
+        py_nvosd_text_param.display_text=" Tran Dinh Manh Cuong TOW2022"
+        py_nvosd_text_param.x_offset=10
+        py_nvosd_text_param.y_offset=12
+        py_nvosd_text_param.font_params.font_size = 20
+        py_nvosd_text_param.font_params.font_name="Serif"
+        #set color
+        py_nvosd_text_param.font_params.font_color.set(1.0,0.5,0.75,0.8)
+        #text background color 
+        py_nvosd_text_param.set_bg_clr=1
+        #set to Black
+        py_nvosd_text_param.text_bg_clr.set(0.2,0.4,0.6,1.0)
+        pyds.nvds_add_display_meta_to_frame(frame_meta,display_meta)
+        try:
+            l_frame=l_frame.next
+        except StopIteration:
+            break
+        #past tracking meta data
+        
+        if(past_tracking_meta[0]==1):
+            l_user=batch_meta.batch_user_meta_list
+        while l_user is not None:
+            try:
+                # Note that l_user.data needs a cast to pyds.NvDsUserMeta
+                # The casting is done by pyds.NvDsUserMeta.cast()
+                # The casting also keeps ownership of the underlying memory
+                # in the C code, so the Python garbage collector will leave
+                # it alone
+                user_meta=pyds.NvDsUserMeta.cast(l_user.data)
+            except StopIteration:
+                break
+            if(user_meta and user_meta.base_meta.meta_type==pyds.NvDsMetaType.NVDS_TRACKER_PAST_FRAME_META):
+                try:
+                    # Note that user_meta.user_meta_data needs a cast to pyds.NvDsPastFrameObjBatch
+                    # The casting is done by pyds.NvDsPastFrameObjBatch.cast()
+                    # The casting also keeps ownership of the underlying memory
+                    # in the C code, so the Python garbage collector will leave
+                    # it alone
+                    pPastFrameObjBatch = pyds.NvDsPastFrameObjBatch.cast(user_meta.user_meta_data)
+                except StopIteration:
+                    break
+                for trackobj in pyds.NvDsPastFrameObjBatch.list(pPastFrameObjBatch):
+                    print("streamId=",trackobj.streamID)
+                    print("surfaceStreamID=",trackobj.surfaceStreamID)
+                    for pastframeobj in pyds.NvDsPastFrameObjStream.list(trackobj):
+                        print("numobj=",pastframeobj.numObj)
+                        print("uniqueId=",pastframeobj.uniqueId)
+                        print("classId=",pastframeobj.classId)
+                        print("objLabel=",pastframeobj.objLabel)
+                        for objlist in pyds.NvDsPastFrameObjList.list(pastframeobj):
+                            print('frameNum:', objlist.frameNum)
+                            print('tBbox.left:', objlist.tBbox.left)
+                            print('tBbox.width:', objlist.tBbox.width)
+                            print('tBbox.top:', objlist.tBbox.top)
+                            print('tBbox.right:', objlist.tBbox.height)
+                            print('confidence:', objlist.confidence)
+                            print('age:', objlist.age)
+            stream_index="stream{0}".format(frame_meta.pad_index) 
+            global perf_data
+            perf_data.update_fps(stream_index)
+            try:
+                l_user=l_user.next
+            except StopIteration:
+                break
+    return Gst.PadProbeReturn.OK
+
+
+
+
+def cb_newpad(decodebin, decoder_src_pad, data):
+    print("In cb_newpad\n")
+    caps = decoder_src_pad.get_current_caps()
+    gststruct = caps.get_structure(0)
+    gstname = gststruct.get_name()
+    source_bin = data
+    features = caps.get_features(0)
+
+    # Need to check if the pad created by the decodebin is for video and not
+    # audio.
+    if (gstname.find("video") != -1):
+        # Link the decodebin pad only if decodebin has picked nvidia
+        # decoder plugin nvdec_*. We do this by checking if the pad caps contain
+        # NVMM memory features.
+        if features.contains("memory:NVMM"):
+            # Get the source bin ghost pad
+            bin_ghost_pad = source_bin.get_static_pad("src")
+            if not bin_ghost_pad.set_target(decoder_src_pad):
+                sys.stderr.write("Failed to link decoder src pad to source bin ghost pad\n")
+        else:
+            sys.stderr.write(" Error: Decodebin did not pick nvidia decoder plugin.\n")
+
+
+def decodebin_child_added(child_proxy, Object, name, user_data):
+    print("Decodebin child added:", name, "\n")
+    if name.find("decodebin") != -1:
+        Object.connect("child-added", decodebin_child_added, user_data)
+
+    if "source" in name:
+        source_element = child_proxy.get_by_name("source")
+        if source_element.find_property('drop-on-latency') != None:
+            Object.set_property("drop-on-latency", True)
+
+def create_source_bin(index, uri):
+    print("Creating source bin")
+
+    # Create a source GstBin to abstract this bin's content from the rest of the
+    # pipeline
+    bin_name = "source-bin-%02d" % index
+    print(bin_name)
+    nbin = Gst.Bin.new(bin_name)
+    if not nbin:
+        sys.stderr.write(" Unable to create source bin \n")
+
+    # Source element for reading from the uri.
+    # We will use decodebin and let it figure out the container format of the
+    # stream and the codec and plug the appropriate demux and decode plugins.
+    uri_decode_bin = Gst.ElementFactory.make("uridecodebin", "uri-decode-bin")
+    if not uri_decode_bin:
+        sys.stderr.write(" Unable to create uri decode bin \n")
+    # We set the input uri to the source element
+    uri_decode_bin.set_property("uri", uri)
+    # Connect to the "pad-added" signal of the decodebin which generates a
+    # callback once a new pad for raw data has beed created by the decodebin
+    uri_decode_bin.connect("pad-added", cb_newpad, nbin)
+    uri_decode_bin.connect("child-added", decodebin_child_added, nbin)
+
+    # We need to create a ghost pad for the source bin which will act as a proxy
+    # for the video decoder src pad. The ghost pad will not have a target right
+    # now. Once the decode bin creates the video decoder and generates the
+    # cb_newpad callback, we will set the ghost pad target to the video decoder
+    # src pad.
+    Gst.Bin.add(nbin, uri_decode_bin)
+    bin_pad = nbin.add_pad(Gst.GhostPad.new_no_target("src", Gst.PadDirection.SRC))
+    if not bin_pad:
+        sys.stderr.write(" Failed to add ghost pad in source bin \n")
+        return None
+    return nbin
+def main(args):
+    #check agrs
+    if len(args) < 2:
+        sys.stderr.write("usage: %s <uri1> [uri2] ... [uriN] <folder to save frames>\n" % args[0])
+        sys.exit(1)
+    if(len(args)==3):
+        past_tracking_meta[0]=int(args[2])
+
+    global perf_data
+    perf_data = PERF_DATA(len(args) - 1)
+    number_sources = len(args) - 1
+    Gst.init(None)
+
+    # Create gstreamer elements */
+    # Create Pipeline element that will form a connection of other elements
+    print("Creating Pipeline \n ")
+    pipeline = Gst.Pipeline()
+    is_live = False
+
+    if not pipeline:
+        sys.stderr.write(" Unable to create Pipeline \n")
+    print("Creating streamux \n ")
+
+    # Create nvstreammux instance to form batches from one or more sources.
+    streammux = Gst.ElementFactory.make("nvstreammux", "Stream-muxer")
+    if not streammux:
+        sys.stderr.write(" Unable to create NvStreamMux \n")
+
+    pipeline.add(streammux)
+    for i in range(number_sources):
+        # os.mkdir(folder_name + "/stream_" + str(i))
+        frame_count["stream_" + str(i)] = 0
+        saved_count["stream_" + str(i)] = 0
+        print("Creating source_bin ", i, " \n ")
+        uri_name = args[i + 1]
+        if uri_name.find("rtsp://") == 0:
+            is_live = True
+        source_bin = create_source_bin(i, uri_name)
+        if not source_bin:
+            sys.stderr.write("Unable to create source bin \n")
+        pipeline.add(source_bin)
+        padname = "sink_%u" % i
+        sinkpad = streammux.get_request_pad(padname)
+        if not sinkpad:
+            sys.stderr.write("Unable to create sink pad bin \n")
+        srcpad = source_bin.get_static_pad("src")
+        if not srcpad:
+            sys.stderr.write("Unable to create src pad bin \n")
+        srcpad.link(sinkpad)
+    print("Creating Pgie \n ")
+    pgie = Gst.ElementFactory.make("nvinfer", "primary-inference")
+    if not pgie:
+        sys.stderr.write(" Unable to create pgie \n")
+    tracker=Gst.ElementFactory.make("nvtracker","tracker")
+    if not tracker:
+        sys.stderr.write(" Unable to create tracker \n")
+    sgie1=Gst.ElementFactory.make("nvinfer","secondary1-nvinference-engine")
+    if not sgie1:
+        sys.stderr.write(" Unable to make sgie1 \n")
+
+
+    sgie2 = Gst.ElementFactory.make("nvinfer", "secondary2-nvinference-engine")
+    if not sgie2:
+        sys.stderr.write(" Unable to make sgie2 \n")
+
+    sgie3 = Gst.ElementFactory.make("nvinfer", "secondary3-nvinference-engine")
+    if not sgie3:
+        sys.stderr.write(" Unable to make sgie3 \n")
+    # Add nvvidconv1 and filter1 to convert the frames to RGBA
+    # which is easier to work with in Python.
+    print("Creating nvvidconv1 \n ")
+    nvvidconv1 = Gst.ElementFactory.make("nvvideoconvert", "convertor1")
+    if not nvvidconv1:
+        sys.stderr.write(" Unable to create nvvidconv1 \n")
+    print("Creating filter1 \n ")
+    caps1 = Gst.Caps.from_string("video/x-raw(memory:NVMM), format=RGBA")
+    filter1 = Gst.ElementFactory.make("capsfilter", "filter1")
+    if not filter1:
+        sys.stderr.write(" Unable to get the caps filter1 \n")
+    filter1.set_property("caps", caps1)
+    print("Creating tiler \n ")
+    tiler = Gst.ElementFactory.make("nvmultistreamtiler", "nvtiler")
+    if not tiler:
+        sys.stderr.write(" Unable to create tiler \n")
+    print("Creating nvvidconv \n ")
+    nvvidconv = Gst.ElementFactory.make("nvvideoconvert", "convertor")
+    if not nvvidconv:
+        sys.stderr.write(" Unable to create nvvidconv \n")
+    print("Creating nvosd \n ")
+    nvosd = Gst.ElementFactory.make("nvdsosd", "onscreendisplay")
+    if not nvosd:
+        sys.stderr.write(" Unable to create nvosd \n")
+    if (is_aarch64()):
+        print("Creating transform \n ")
+        transform = Gst.ElementFactory.make("nvegltransform", "nvegl-transform")
+        if not transform:
+            sys.stderr.write(" Unable to create transform \n")
+
+    print("Creating EGLSink \n")
+    sink = Gst.ElementFactory.make("nveglglessink", "nvvideo-renderer")
+    if not sink:
+        sys.stderr.write(" Unable to create egl sink \n")
+
+    if is_live:
+        print("Atleast one of the sources is live")
+        streammux.set_property('live-source', 1)
+
+    streammux.set_property('width', 1280)
+    streammux.set_property('height', 720)
+    streammux.set_property('batch-size', number_sources)
+    streammux.set_property('batched-push-timeout', 4000000)
+    pgie.set_property('config-file-path', "buffer2numpy.txt")
+    sgie1.set_property('config-file-path', "dstest2_sgie1_config.txt")
+    sgie2.set_property('config-file-path', "dstest2_sgie2_config.txt")
+    sgie3.set_property('config-file-path', "dstest2_sgie3_config.txt")
+    
+    #Set properties of tracker
+    config = configparser.ConfigParser()
+    config.read('dstest2_tracker_config.txt')
+    config.sections()
+
+    for key in config['tracker']:
+        if key == 'tracker-width' :
+            tracker_width = config.getint('tracker', key)
+            tracker.set_property('tracker-width', tracker_width)
+        if key == 'tracker-height' :
+            tracker_height = config.getint('tracker', key)
+            tracker.set_property('tracker-height', tracker_height)
+        if key == 'gpu-id' :
+            tracker_gpu_id = config.getint('tracker', key)
+            tracker.set_property('gpu_id', tracker_gpu_id)
+        if key == 'll-lib-file' :
+            tracker_ll_lib_file = config.get('tracker', key)
+            tracker.set_property('ll-lib-file', tracker_ll_lib_file)
+        if key == 'll-config-file' :
+            tracker_ll_config_file = config.get('tracker', key)
+            tracker.set_property('ll-config-file', tracker_ll_config_file)
+        if key == 'enable-batch-process' :
+            tracker_enable_batch_process = config.getint('tracker', key)
+            tracker.set_property('enable_batch_process', tracker_enable_batch_process)
+        if key == 'enable-past-frame' :
+            tracker_enable_past_frame = config.getint('tracker', key)
+            tracker.set_property('enable_past_frame', tracker_enable_past_frame)
+    pgie_batch_size = pgie.get_property("batch-size")
+    if (pgie_batch_size != number_sources):
+        print("WARNING: Overriding infer-config batch-size", pgie_batch_size, " with number of sources ",
+              number_sources, " \n")
+        pgie.set_property("batch-size", number_sources)
+    tiler_rows = int(math.sqrt(number_sources))
+    tiler_columns = int(math.ceil((1.0 * number_sources) / tiler_rows))
+    tiler.set_property("rows", tiler_rows)
+    tiler.set_property("columns", tiler_columns)
+    tiler.set_property("width", TILED_OUTPUT_WIDTH)
+    tiler.set_property("height", TILED_OUTPUT_HEIGHT)
+
+    sink.set_property("sync", 0)
+    sink.set_property("qos", 0)
+
+    if not is_aarch64():
+        # Use CUDA unified memory in the pipeline so frames
+        # can be easily accessed on CPU in Python.
+        mem_type = int(pyds.NVBUF_MEM_CUDA_UNIFIED)
+        streammux.set_property("nvbuf-memory-type", mem_type)
+        nvvidconv.set_property("nvbuf-memory-type", mem_type)
+        nvvidconv1.set_property("nvbuf-memory-type", mem_type)
+        tiler.set_property("nvbuf-memory-type", mem_type)
+
+    print("Adding elements to Pipeline \n")
+    pipeline.add(pgie)
+    pipeline.add(tiler)
+    pipeline.add(nvvidconv)
+    pipeline.add(filter1)
+    pipeline.add(tracker)
+    pipeline.add(sgie1)
+    pipeline.add(sgie2)
+    pipeline.add(sgie3)
+    pipeline.add(nvvidconv1)
+    pipeline.add(nvosd)
+    if is_aarch64():
+        pipeline.add(transform)
+    pipeline.add(sink)
+
+    print("Linking elements in the Pipeline \n")
+    streammux.link(pgie)
+    pgie.link(tracker)
+    tracker.link(sgie1)
+    sgie1.link(sgie2)
+    sgie2.link(sgie3)
+    sgie3.link(nvvidconv)
+    nvvidconv1.link(filter1)
+    filter1.link(tiler)
+    tiler.link(nvvidconv)
+    nvvidconv.link(nvosd)
+    if is_aarch64():
+        nvosd.link(transform)
+        transform.link(sink)
+    else:
+        nvosd.link(sink)
+
+    # create an event loop and feed gstreamer bus mesages to it
+    loop = GLib.MainLoop()
+    bus = pipeline.get_bus()
+    bus.add_signal_watch()
+    bus.connect("message", bus_call, loop)
+
+    tiler_sink_pad = tiler.get_static_pad("sink")
+    if not tiler_sink_pad:
+        sys.stderr.write(" Unable to get src pad \n")
+    else:
+        tiler_sink_pad.add_probe(Gst.PadProbeType.BUFFER,buffer2numpy_image_probe, 0)
+        # perf callback function to print fps every 5 sec
+        GLib.timeout_add(5000, perf_data.perf_print_callback)
+
+    # List the sources
+    print("Now playing...")
+    for i, source in enumerate(args[:-1]):
+        if i != 0:
+            print(i, ": ", source)
+
+    print("Starting pipeline \n")
+    # start play back and listed to events		
+    pipeline.set_state(Gst.State.PLAYING)
+    try:
+        loop.run()
+    except:
+        pass
+    # cleanup
+    print("Exiting app\n")
+    pipeline.set_state(Gst.State.NULL)
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
+
+    
+            
+                               
